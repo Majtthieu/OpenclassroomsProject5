90a1fb8bb4c5b2422ffa0ea2f8461b036abd4419
Majtthieu <matthieu.jankowiak@yahoo.fr>
2023-12-28 22:29:00 +0100

    creation serveur node express

---
+++ routes/book.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+
+const booksCtrl = require('../controllers/book');
+
+const auth = require('../middleware/auth');
+
+const multer = require('../middleware/multer-config')
+
+router.get('/', auth, booksCtrl.getAllBooks);
+router.post('/', auth, multer, booksCtrl.createBook);
+router.get('/:id', auth, booksCtrl.getOneBook);
+router.put('/:id', auth, multer, booksCtrl.modifyBook);
+router.delete('/:id', auth, booksCtrl.deleteBook);
+
+module.exports = router;
